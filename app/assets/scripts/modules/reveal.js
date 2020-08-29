86825a322059cd8ba7142fe3137fe3733834ea21
Talha Imran <talhaimran284@gmail.com>
2020-08-29 08:49:34 +0500

    Installed webpack and webpack-cli

--- app/assets/scripts/modules/reveal.js
+++ app/assets/scripts/modules/reveal.js
@@ -9,12 +9,10 @@ export default RevealOnScroll; */
 
 
 
-class Person{
-    constructor(name, age){
-        this.name = name;
-        this.age = age;
-    }
-
+function Person(name, age){
+    
+    this.name = name;
+    this.age = age;
 
     bio = function(){
         console.log(`My name is ${this.name} and my age is ${this.age}`);
